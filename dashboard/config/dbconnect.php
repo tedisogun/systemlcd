a769acc99dfd0b8012f910623df14e81f69c2fe2
Sogun <sogun.kwn@gmail.com>
2019-02-09 14:59:08 +0800

    Update dbconnect.php

--- dashboard/config/dbconnect.php
+++ dashboard/config/dbconnect.php
@@ -2,7 +2,7 @@
 
 $host     = "localhost";
 $db_username_ppp = "root";
-$db_password_db = "Mylinggih14";
+$db_password_db = "";
 $dbname   = "systemlcd";
 
 $dbc = mysqli_connect($host,$db_username_ppp,$db_password_db,$dbname) or die("koneksi database gagal");
